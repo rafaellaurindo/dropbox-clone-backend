79ea251a769248d83edde3f065e3f5137d90deaf
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-13 22:54:50 -0300

    feat: created route and method to show a box  with your files

--- backend/src/controllers/BoxControler.js
+++ backend/src/controllers/BoxControler.js
@@ -7,6 +7,14 @@ class BoxController {
     });
     res.json(box);
   }
+
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: "files",
+      options: { sort: { createdAt: -1 } }
+    });
+    return res.json(box);
+  }
 }
 
 module.exports = new BoxController();
