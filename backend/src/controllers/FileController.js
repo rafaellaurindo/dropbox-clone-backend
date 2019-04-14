cf808e41245b22ec988058ff0e20028943c86b84
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-13 22:44:51 -0300

    feat: created method to create a new file

--- backend/src/controllers/FileController.js
+++ backend/src/controllers/FileController.js
@@ -1,5 +1,21 @@
 const File = require("../models/File");
+const Box = require("../models/Box");
 
-class FileController {}
+class FileController {
+  async store(req, res) {
+    const box = await Box.findById(req.params.id);
+
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    });
+
+    box.files.push(file);
+
+    await box.save();
+
+    return res.json(file);
+  }
+}
 
 module.exports = new FileController();
