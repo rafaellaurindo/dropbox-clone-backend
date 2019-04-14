4c1bc6e11d524dc88b244ccf74020f372a7da16e
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-13 22:10:20 -0300

    feat: add connection with mongo db instance

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+const MONGODB_URI = "YOUR_MONGODB_INSTANCE_URI_HERE";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.use(express.json());
 app.use(
