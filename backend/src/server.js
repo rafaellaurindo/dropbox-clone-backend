e45b6f44c761bf876fc0e5914a4255c28883b5f4
Rafael Laurindo <rafaelfilholm@gmail.com>
2019-04-13 21:34:33 -0300

    syntax: changes in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true
-}));
+  })
+);
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 app.listen(8888);
